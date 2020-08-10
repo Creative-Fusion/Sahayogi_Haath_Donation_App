e1e36135612c4c0424921a32fc5e7b32b7c2cb11
nistasimkhada977 <58097961+nistasimkhada977@users.noreply.github.com>
2020-08-10 10:38:56 +0545

    images1 folder added

--- lib/screens/activities/activitiesScreen.dart
+++ lib/screens/activities/activitiesScreen.dart
@@ -8,7 +8,7 @@ class ActivitiesScreen extends StatefulWidget {
 }
 
 class _ActivitiesScreenState extends State<ActivitiesScreen> {
-  bool isFavorite = false;
+  bool isFavorite = true;
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
@@ -29,7 +29,7 @@ class _ActivitiesScreenState extends State<ActivitiesScreen> {
                   height: mediaQuery.size.height * 0.50,
                   decoration: BoxDecoration(
                     image: DecorationImage(
-                      image: AssetImage('images/children.jpg'),
+                      image: AssetImage('assets/images1/children.jpg'),
                       fit: BoxFit.cover,
                     ),
                     borderRadius: BorderRadius.only(
@@ -90,7 +90,7 @@ class _ActivitiesScreenState extends State<ActivitiesScreen> {
                     child: ClipOval(
                       child: Image(
                         height: mediaQuery.size.height * 0.09,
-                        image: AssetImage('images/children.jpg'),
+                        image: AssetImage('assets/images1/children.jpg'),
                         fit: BoxFit.cover,
                       ),
                     ),
@@ -119,22 +119,11 @@ class _ActivitiesScreenState extends State<ActivitiesScreen> {
                 child: Text(
                   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                   style: kFinalTextStyle,
-//
-//              Firestore.instance.collection('activities/
-// 2dnhSjQrkyCKwZBGREUE
-// ').snapshots().listen((data){
-//                data.document.forEach((document){
-// print(document['description']);
-//                });
 
-// });
 
-//
-//
-                )
 
-                // maxLines: 9,
                 ),
+            ),
             // // overflow: TextOverflow.ellipsis,
           ],
         ),
